a6abd83f730e2aebda3043c9f9a22915b9a1baaf
chriscarlton5 <chrislcarlton@gmail.com>
2026-04-28 17:04:55 -0500

    auto: lightgbm_ft_0003 - Gini 0.1835

--- autoresearch/train.py
+++ autoresearch/train.py
@@ -8,31 +8,31 @@ from prepare import run_experiment
 
 
 CANDIDATE = {
-  "id": "lightgbm_ft_0002",
+  "id": "lightgbm_ft_0003",
   "is_baseline": False,
   "model_type": "lightgbm",
-  "description": "Fine-tune: l2. FF=0.512, L2=13.1",
-  "hypothesis": "Targeted l2 variation",
+  "description": "Fine-tune: leaves. FF=0.512, L2=13.5",
+  "hypothesis": "Targeted leaves variation",
   "actuarial_rationale": "Exploiting sweet spot",
   "lightgbm": {
     "nrounds": 178,
     "early_stopping_rounds": 22,
     "frequency_grid": [
       {
-        "num_leaves": 17,
+        "num_leaves": 15,
         "min_data_in_leaf": 1700,
         "learning_rate": 0.036,
         "feature_fraction": 0.5123,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 13.098559915487673
+        "lambda_l2": 13.4675
       },
       {
-        "num_leaves": 25,
+        "num_leaves": 23,
         "min_data_in_leaf": 1360,
         "learning_rate": 0.030999999999999996,
         "feature_fraction": 0.4623,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 11.098559915487673
+        "lambda_l2": 11.4675
       }
     ],
     "severity_grid": [
@@ -42,7 +42,7 @@ CANDIDATE = {
         "learning_rate": 0.036,
         "feature_fraction": 0.5123,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 10.098559915487673
+        "lambda_l2": 10.4675
       },
       {
         "num_leaves": 11,
@@ -50,7 +50,7 @@ CANDIDATE = {
         "learning_rate": 0.030999999999999996,
         "feature_fraction": 0.4623,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 9.098559915487673
+        "lambda_l2": 9.4675
       }
     ],
     "capped_severity_grid": [
@@ -60,7 +60,7 @@ CANDIDATE = {
         "learning_rate": 0.036,
         "feature_fraction": 0.5123,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 10.098559915487673
+        "lambda_l2": 10.4675
       },
       {
         "num_leaves": 11,
@@ -68,7 +68,7 @@ CANDIDATE = {
         "learning_rate": 0.030999999999999996,
         "feature_fraction": 0.4623,
         "bagging_fraction": 0.5123,
-        "lambda_l2": 9.098559915487673
+        "lambda_l2": 9.4675
       }
     ]
   }
